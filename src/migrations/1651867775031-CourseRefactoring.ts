87e25506987a419226da6adb216cbcf336557d36
Gabriel Azevedo <gabrielazevedo492@gmail.com>
2022-05-06 23:55:50 -0300

    Curso NestJS

---
+++ src/migrations/1651867775031-CourseRefactoring.ts
@@ -0,0 +1,13 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class CourseRefactoring1651867775031 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "courses" RENAME COLUMN "name" TO "course"`)
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "courses" RENAME COLUMN "course" TO "name"`)
+    }
+
+}
